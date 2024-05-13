ce4e8a497e22697090562082fc2d75c69a3d982e
NEMYSESx <111594052+NEMYSESx@users.noreply.github.com>
2024-05-13 14:24:41 +0530

    fix6

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -3,30 +3,24 @@ import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
 export const POST = async (req: NextRequest) => {
+  const reqBody = await req.json();
+  const { email, password } = reqBody;
   try {
-    const reqBody = await req.json();
-    const { email, password } = reqBody;
-    try {
-      await signIn("credentials", {
-        email,
-        password,
-        redirectTo: DEFAULT_LOGIN_REDIRECT,
-      });
-      return;
-    } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.type) {
-          case "CredentialsSignin":
-            return { error: "Invalid credentials" };
-          default:
-            return { error: "Soemthing went wrong" };
-        }
-      }
-      throw error;
-    }
-  } catch (error: any) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 401,
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
     });
+    return;
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+        default:
+          return { error: "Soemthing went wrong" };
+      }
+    }
+    throw error;
   }
 };
